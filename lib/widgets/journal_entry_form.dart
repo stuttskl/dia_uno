ae234203af8759f9a545983214242e114efa7c01
Katie <25576005+stuttskl@users.noreply.github.com>
2021-02-25 08:57:10 -0700

    implemented form validation for all fields

--- lib/widgets/journal_entry_form.dart
+++ lib/widgets/journal_entry_form.dart
@@ -7,8 +7,13 @@ class JournalEntryForm extends StatefulWidget {
 
 class _JournalEntryFormState extends State<JournalEntryForm> {
   void saveEntry(BuildContext context) {
-    print("Save new entry");
-    Navigator.of(context).pop();
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print("valid, saving");
+      Navigator.of(context).pop();
+
+    }
+    print("invalid, not saving");
   }
 
   void cancelEntry(BuildContext context) {
@@ -22,45 +27,79 @@ class _JournalEntryFormState extends State<JournalEntryForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            autofocus: true,
-            decoration: InputDecoration(
-              labelText: 'Title',
-              border: OutlineInputBorder(),
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                labelText: 'Title',
+                border: OutlineInputBorder(),
+              ),
+              onSaved: (value) {
+                // store value in some object
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-          ),
-          TextFormField(
-            autofocus: true,
-            decoration: InputDecoration(
-              labelText: 'Body',
-              border: OutlineInputBorder(),
+            SizedBox(height: 20),
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                labelText: 'Body',
+                border: OutlineInputBorder(),
+              ),
+              onSaved: (value) {
+                // store value in some object
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-          ),
-          TextFormField(
-            autofocus: true,
-            decoration: InputDecoration(
-              labelText: 'Rating',
-              border: OutlineInputBorder(),
+            SizedBox(height: 20),
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                labelText: 'Rating',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+              onSaved: (value) {
+                // store value in some object
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-            keyboardType: TextInputType.number,
-          ),
-          InputDatePickerFormField(
-            autofocus: true,
-            firstDate: DateTime.now(),
-            lastDate: DateTime.now(),
-            
-          ),
-          RaisedButton(
-            onPressed: () => saveEntry(context),
-            child: Text('Save'),
-          ),
-          RaisedButton(
-            onPressed: () => cancelEntry(context),
-            child: Text('Cancel'),
-          ),
-        ]
+            SizedBox(height: 20),
+            InputDatePickerFormField(
+              autofocus: true,
+              firstDate: DateTime.now(),
+              lastDate: DateTime.now(),
+            ),
+            SizedBox(height: 20),
+            RaisedButton(
+              onPressed: () => saveEntry(context),
+              child: Text('Save'),
+            ),
+            RaisedButton(
+              onPressed: () => cancelEntry(context),
+              child: Text('Cancel'),
+            ),
+          ]
+      )
       )
     );
   }
